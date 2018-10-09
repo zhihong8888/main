a9f52e6a7946e927a402c6c4cc2fe2d1120ccb6c
zhihong8888 <zhihong8888@gmail.com>
2018-10-09 22:56:16 +0800

    Travis fix

--- src/main/java/seedu/address/model/schedule/exceptions/DuplicateScheduleException.java
+++ src/main/java/seedu/address/model/schedule/exceptions/DuplicateScheduleException.java
@@ -1,5 +1,9 @@
 package seedu.address.model.schedule.exceptions;
 
+/**
+ * Signals that the operation will result in duplicate Schedules (Schedules are considered duplicates if they have the same
+ * identity).
+ */
 public class DuplicateScheduleException extends RuntimeException {
     public DuplicateScheduleException() {
         super("Operation would result in duplicate Schedule");
