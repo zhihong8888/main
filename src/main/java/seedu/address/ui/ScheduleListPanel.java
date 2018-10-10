caf33ea73c30e807f7755be829869703aa3ab118
zhihong8888 <zhihong8888@gmail.com>
2018-10-10 22:56:29 +0800

    codacy checkstyle fix

--- src/main/java/seedu/address/ui/ScheduleListPanel.java
+++ src/main/java/seedu/address/ui/ScheduleListPanel.java
@@ -2,16 +2,12 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
-import com.google.common.eventbus.Subscribe;
-
-import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.commons.events.ui.SchedulePanelSelectionChangedEvent;
 import seedu.address.model.schedule.Schedule;
 
@@ -46,22 +42,6 @@ public class ScheduleListPanel extends UiPart<Region> {
                     }
                 });
     }
-
-    /**
-     * Scrolls to the {@code PersonCard} at the {@code index} and selects it.
-     */
-/*    private void scrollTo(int index) {
-        Platform.runLater(() -> {
-            scheduleListView.scrollTo(index);
-            scheduleListView.getSelectionModel().clearAndSelect(index);
-        });
-    }
-
-    @Subscribe
-    private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        scrollTo(event.targetIndex);
-    }*/
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Schedule} using a {@code ScheduleCard}.
