7be3e09121d6689e04bb27fd5e33198d7cddf3a6
zhihong8888 <zhihong8888@gmail.com>
2018-10-13 15:20:29 +0800

    travis checkstyle fix

--- src/main/java/seedu/address/logic/commands/DeleteScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteScheduleCommand.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
@@ -7,11 +11,10 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.schedule.Schedule;
 
-import java.util.List;
-
-import static java.util.Objects.requireNonNull;
-
-public class DeleteScheduleCommand extends Command{
+/**
+ * Deletes a schedule identified using it's displayed index from the schedule list.
+ */
+public class DeleteScheduleCommand extends Command {
     public static final String COMMAND_WORD = "deleteSchedule";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
