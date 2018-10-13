7ae67d99d62c37d88d4745dbe7da9a90efdadc7e
zhihong8888 <zhihong8888@gmail.com>
2018-10-13 17:48:25 +0800

    find employee displays addressbook and schedule related to employee delete employee deletes  addressbook and schedule related to employee

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -6,6 +6,11 @@ import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+import seedu.address.model.schedule.NricScheduleContainsKeywordsPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
@@ -21,6 +26,8 @@ public class FindCommand extends Command {
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
     private final NameContainsKeywordsPredicate predicate;
+    private NricScheduleContainsKeywordsPredicate predicateNric;
+    List<String> nricList = new ArrayList<String>();
 
     public FindCommand(NameContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
@@ -30,8 +37,17 @@ public class FindCommand extends Command {
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
         model.updateFilteredPersonList(predicate);
+
+        for (Person person : model.getFilteredPersonList()) {
+            nricList.add(person.getEmployeeId().toString());
+        }
+        predicateNric = new NricScheduleContainsKeywordsPredicate(nricList);
+        model.updateFilteredScheduleList(predicateNric);
+
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()) + "\n"
+                    + String.format(Messages.MESSAGE_SCHEDULESS_LISTED_OVERVIEW, model.getFilteredScheduleList().size())
+        );
     }
 
     @Override
