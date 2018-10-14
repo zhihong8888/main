89684d53a3b2547dc7c09071a8567752ddba2414
LimYiSheng <e0176969@u.nus.edu>
2018-10-14 22:28:56 +0800

    Updated FilterCommand to accept prefixes of d/ and r/ for department and position respectively

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -1,12 +1,16 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DEPARTMENT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_POSITION;
 
 import java.util.Arrays;
 
 import seedu.address.logic.commands.FilterCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.DepartmentContainsKeywordsPredicate;
+import seedu.address.model.person.PositionContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FilterCommand object
@@ -20,13 +24,44 @@ public class FilterCommandParser {
      */
 
     public FilterCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim().toUpperCase();
-        if (trimmedArgs.isEmpty()) {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_DEPARTMENT, PREFIX_POSITION);
+
+        if (!argMultimap.getValue(PREFIX_DEPARTMENT).isPresent()
+                && !argMultimap.getValue(PREFIX_POSITION).isPresent()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
         }
 
-        String[] departmentKeywords = trimmedArgs.split("\\s+");
+        String trimmedDepartment = new String(" ");
+        String trimmedPosition = new String(" ");
+        String[] departmentKeywords = new String[]{""};
+        String[] positionKeywords = new String[]{""};
 
-        return new FilterCommand(new DepartmentContainsKeywordsPredicate(Arrays.asList(departmentKeywords)));
+        if (argMultimap.getValue(PREFIX_DEPARTMENT).isPresent()) {
+            trimmedDepartment = (argMultimap.getValue(PREFIX_DEPARTMENT).get()).trim().toUpperCase();
+            departmentKeywords = trimmedDepartment.split("\\s+");
+        }
+
+        if (argMultimap.getValue(PREFIX_POSITION).isPresent()) {
+            trimmedPosition = (argMultimap.getValue(PREFIX_POSITION).get()).trim().toUpperCase();
+            positionKeywords = trimmedPosition.split("\\s+");
+        }
+
+        FilterCommand filterCommand = new FilterCommand(new DepartmentContainsKeywordsPredicate(Arrays
+                .asList(departmentKeywords)), new PositionContainsKeywordsPredicate(Arrays.asList(positionKeywords)));
+
+        if (argMultimap.getValue(PREFIX_DEPARTMENT).isPresent()) {
+            filterCommand.setIsDepartmentPrefixPresent(true);
+        } else if (!argMultimap.getValue(PREFIX_DEPARTMENT).isPresent()) {
+            filterCommand.setIsDepartmentPrefixPresent(false);
+        }
+
+        if (argMultimap.getValue(PREFIX_POSITION).isPresent()) {
+            filterCommand.setIsPositionPrefixPresent(true);
+        } else if (argMultimap.getValue(PREFIX_POSITION).isPresent()) {
+            filterCommand.setIsPositionPrefixPresent(false);
+        }
+
+        return filterCommand;
     }
 }
