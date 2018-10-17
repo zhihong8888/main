b807a3d5298fad41bcf6a7a9647e12c80cd97dfc
ryanchen2018 <cqunming@u.nus.edu>
2018-10-18 03:10:34 +0800

    added back missing tests

--- src/test/java/seedu/address/storage/StorageManagerTest.java
+++ src/test/java/seedu/address/storage/StorageManagerTest.java
@@ -14,6 +14,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import seedu.address.commons.events.model.AddressBookChangedEvent;
 import seedu.address.commons.events.model.ScheduleListChangedEvent;
 import seedu.address.commons.events.storage.DataSavingExceptionEvent;
 import seedu.address.model.UserPrefs;
@@ -85,6 +86,18 @@ public class StorageManagerTest {
     @Test
     public void getAddressBookFilePath() {
         assertNotNull(storageManager.getAddressBookFilePath());
+    }
+
+    @Test
+    public void handleAddressBookChangedEvent_exceptionThrown_eventRaised() {
+        // Create a StorageManager while injecting a stub that  throws an exception when the save method is called
+        Storage storage = new StorageManager(new XmlAddressBookStorageExceptionThrowingStub(Paths.get("dummy")), (
+                new XmlExpensesListStorageExceptionThrowingStub(Paths.get("dummy"))), (
+                new XmlScheduleListStorageExceptionThrowingStub(Paths.get("dummy"))),
+                new XmlRecruitmentListStorageExceptionThrowingStub(Paths.get("dummy")),
+                new JsonUserPrefsStorage(Paths.get("dummy")));
+        storage.handleAddressBookChangedEvent(new AddressBookChangedEvent(new AddressBook()));
+        assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof DataSavingExceptionEvent);
     }
 
     @Test
