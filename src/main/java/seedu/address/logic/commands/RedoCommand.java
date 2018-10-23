191c941780610f55dd4889662ac5486c3dd60d25
zhihong8888 <zhihong8888@gmail.com>
2018-10-24 05:25:58 +0800

    travis checkstyle

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -1,17 +1,13 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EXPENSES;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_RECRUITMENTS;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_SCHEDULES;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.StorageTypes;
-import seedu.address.model.expenses.VersionedExpensesList;
-import seedu.address.model.recruitment.VersionedRecruitmentList;
 import seedu.address.model.schedule.VersionedScheduleList;
 
 /**
@@ -23,19 +19,19 @@ public class RedoCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Redo success!";
     public static final String MESSAGE_FAILURE = "No more commands to redo!";
 
-    int loop_count;
+    private int loop_count;
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
-        if(model.getDeletedPersonUndoRedoLoop()) {
+        if (model.getDeletedPersonUndoRedoLoop()) {
             loop_count = DeleteCommand.NUM_STORAGE_DELETES;
         } else {
             loop_count = 1;
         }
 
-        while(loop_count > 0) {
+        while (loop_count > 0) {
             loop_count--;
 
             if (!model.canRedoStorage()) {
@@ -44,32 +40,32 @@ public class RedoCommand extends Command {
             StorageTypes storage = model.getNextCommitType();
 
             switch (storage) {
-                case ADDRESS_BOOK:
-                    if (!model.canRedoAddressBook()) {
-                        throw new CommandException(MESSAGE_FAILURE);
-                    }
-                    try {
-                        model.redoAddressBook();
-                        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-                    } catch (VersionedScheduleList.NoRedoableStateException e) {
-                        throw new CommandException(MESSAGE_FAILURE);
-                    }
-                    break;
+            case ADDRESS_BOOK:
+                if (!model.canRedoAddressBook()) {
+                    throw new CommandException(MESSAGE_FAILURE);
+                }
+                try {
+                    model.redoAddressBook();
+                    model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+                } catch (VersionedScheduleList.NoRedoableStateException e) {
+                    throw new CommandException(MESSAGE_FAILURE);
+                }
+                break;
 
-                case SCHEDULES_LIST:
-                    if (!model.canRedoScheduleList()) {
-                        throw new CommandException(MESSAGE_FAILURE);
-                    }
-                    try {
-                        model.redoScheduleList();
-                        model.updateFilteredScheduleList(PREDICATE_SHOW_ALL_SCHEDULES);
-                    } catch (VersionedScheduleList.NoRedoableStateException e) {
-                        throw new CommandException(MESSAGE_FAILURE);
-                    }
-                    break;
+            case SCHEDULES_LIST:
+                if (!model.canRedoScheduleList()) {
+                    throw new CommandException(MESSAGE_FAILURE);
+                }
+                try {
+                    model.redoScheduleList();
+                    model.updateFilteredScheduleList(PREDICATE_SHOW_ALL_SCHEDULES);
+                } catch (VersionedScheduleList.NoRedoableStateException e) {
+                    throw new CommandException(MESSAGE_FAILURE);
+                }
+                break;
 
-                default:
-                    break;
+            default:
+                break;
             }
         }
 
--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,17 +1,13 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EXPENSES;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_RECRUITMENTS;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_SCHEDULES;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.StorageTypes;
-import seedu.address.model.expenses.VersionedExpensesList;
-import seedu.address.model.recruitment.VersionedRecruitmentList;
 import seedu.address.model.schedule.VersionedScheduleList;
 
 /**
@@ -29,13 +25,13 @@ public class UndoCommand extends Command {
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
-        if(model.getDeletedPersonUndoRedoLoop()) {
+        if (model.getDeletedPersonUndoRedoLoop()) {
             loop_count = DeleteCommand.NUM_STORAGE_DELETES;
         } else {
             loop_count = 1;
         }
 
-        while(loop_count > 0) {
+        while (loop_count > 0) {
             loop_count --;
 
             if (!model.canUndoStorage()) {
@@ -44,33 +40,32 @@ public class UndoCommand extends Command {
             StorageTypes storage = model.getLastCommitType();
 
             switch(storage) {
+            case SCHEDULES_LIST:
+                if (!model.canUndoScheduleList()) {
+                    throw new CommandException(MESSAGE_FAILURE);
+                }
+                try {
+                    model.undoScheduleList();
+                    model.updateFilteredScheduleList(PREDICATE_SHOW_ALL_SCHEDULES);
+                } catch (VersionedScheduleList.NoRedoableStateException e) {
+                    throw new CommandException(MESSAGE_FAILURE);
+                }
+                break;
 
-                case SCHEDULES_LIST:
-                    if (!model.canUndoScheduleList()) {
-                        throw new CommandException(MESSAGE_FAILURE);
-                    }
-                    try {
-                        model.undoScheduleList();
-                        model.updateFilteredScheduleList(PREDICATE_SHOW_ALL_SCHEDULES);
-                    } catch (VersionedScheduleList.NoRedoableStateException e) {
-                        throw new CommandException(MESSAGE_FAILURE);
-                    }
-                    break;
+            case ADDRESS_BOOK:
+                if (!model.canUndoAddressBook()) {
+                    throw new CommandException(MESSAGE_FAILURE);
+                }
+                try {
+                    model.undoAddressBook();
+                    model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+                } catch (VersionedScheduleList.NoRedoableStateException e) {
+                    throw new CommandException(MESSAGE_FAILURE);
+                }
+                break;
 
-                case ADDRESS_BOOK:
-                    if (!model.canUndoAddressBook()) {
-                        throw new CommandException(MESSAGE_FAILURE);
-                    }
-                    try {
-                        model.undoAddressBook();
-                        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-                    } catch (VersionedScheduleList.NoRedoableStateException e) {
-                        throw new CommandException(MESSAGE_FAILURE);
-                    }
-                    break;
-
-                default:
-                    break;
+            default:
+                break;
             }
         }
 
