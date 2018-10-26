14dd67a23077c2111a3ee3b15432ac02764a44a1
ryanchen2018 <cqunming@u.nus.edu>
2018-10-26 15:23:59 +0800

    travis fix.

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,8 +10,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_EXPENSES_DISPLAYED_INDEX = "The expenses index provided is invalid";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_INVALID_SCHEDULE_DISPLAYED_INDEX = "The schedule index provided is invalid";
-    public static final String MESSAGE_INVALID_RECRUITMENT_POST_DISPLAYED_INDEX = "The recruitmentPost " +
-            "index provided is invalid";
+    public static final String MESSAGE_INVALID_RECRUITMENT_POST_DISPLAYED_INDEX = "The recruitmentPost "
+            + "index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_SCHEDULESS_LISTED_OVERVIEW = "%1$d schedules listed!";
 
