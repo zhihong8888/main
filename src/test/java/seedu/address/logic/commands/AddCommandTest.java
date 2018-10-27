078b2094ac17dab8fe3759e1d04b23b385c2fe9e
zhihong8888 <zhihong8888@gmail.com>
2018-10-27 20:30:25 +0800

    travis

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Set;
 
 import java.util.function.Predicate;
