5d2317a43f1d53b5ef7cc8b48ff4a8670a910747
LimYiSheng <e0176969@u.nus.edu>
2018-10-29 23:00:05 +0800

    Removed unused import

--- src/test/java/systemtests/EditCommandSystemTest.java
+++ src/test/java/systemtests/EditCommandSystemTest.java
@@ -20,7 +20,6 @@ import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_HUSBAND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
