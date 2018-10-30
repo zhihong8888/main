a08a6d6348729845abc749b9722ec320c6d09b6e
LimYiSheng <e0176969@u.nus.edu>
2018-10-31 01:48:44 +0800

    FindCommand can now find either by employeeId or name

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -5,6 +5,9 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
+import seedu.address.model.person.EmployeeId;
+import seedu.address.model.person.EmployeeIdContainsKeywordsPredicate;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 /**
@@ -15,21 +18,44 @@ public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all person(s) whose name(s) matches the input "
+            + "(case-insensitive) or find the person whose employee ID matches the input and displays them as a list "
+            + "with index number.\nParameters: NAME OR EMPLOYEEID\n"
+            + "Example: " + COMMAND_WORD + " Alex yeoh\n" + "Example: " + COMMAND_WORD + " 000001";
 
-    private final NameContainsKeywordsPredicate predicate;
+    public static final String MESSAGE_VALID_INPUT = "Please enter either a valid name or valid employeeId\n"
+            + Name.MESSAGE_NAME_CONSTRAINTS + "\n"
+            + EmployeeId.MESSAGE_EMPLOYEEID_CONSTRAINTS;
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
-        this.predicate = predicate;
+    private final NameContainsKeywordsPredicate namePredicate;
+    private final EmployeeIdContainsKeywordsPredicate employeeIdPredicate;
+    private boolean isInputName;
+    private boolean isInputEmployeeId;
+
+    public FindCommand(NameContainsKeywordsPredicate namePredicate,
+                       EmployeeIdContainsKeywordsPredicate employeeIdPredicate) {
+        this.namePredicate = namePredicate;
+        this.employeeIdPredicate = employeeIdPredicate;
+    }
+
+    public void setIsInputName(boolean isInputName) {
+        this.isInputName = isInputName;
+    }
+
+    public void setIsInputEmployeeId(boolean isInputEmployeeId) {
+        this.isInputEmployeeId = isInputEmployeeId;
     }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
-        model.updateFilteredPersonList(predicate);
+
+        if (isInputName && !isInputEmployeeId) {
+            model.updateFilteredPersonList(namePredicate);
+        } else if (!isInputName && isInputEmployeeId) {
+            model.updateFilteredPersonList(employeeIdPredicate);
+        }
+
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
@@ -38,6 +64,7 @@ public class FindCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof FindCommand // instanceof handles nulls
-                && predicate.equals(((FindCommand) other).predicate)); // state check
+                && (namePredicate.equals(((FindCommand) other).namePredicate)
+                || employeeIdPredicate.equals(((FindCommand) other).employeeIdPredicate))); // state check
     }
 }
--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,33 +1,95 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-
-import java.util.Arrays;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+import seedu.address.model.person.EmployeeId;
+import seedu.address.model.person.EmployeeIdContainsKeywordsPredicate;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Parses input arguments and creates a new FindCommand object
  */
 public class FindCommandParser implements Parser<FindCommand> {
 
+    private static final int INDEX_FIRST_CHARACTER = 1;
+    private boolean isInputName = false;
+    private boolean isInputEmployeeId = false;
+
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns an FindCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
+        requireNonNull(args);
+
+        String keyword = args.trim();
+        if (keyword.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        FindCommand findCommand = new FindCommand(new NameContainsKeywordsPredicate(keyword),
+                new EmployeeIdContainsKeywordsPredicate(keyword));
+        isNameOrEmployeeId(keyword);
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        if (!isInputName && !isInputEmployeeId) {
+            throw new ParseException(FindCommand.MESSAGE_VALID_INPUT);
+        } else if (isInputName && !isInputEmployeeId) {
+            findCommand.setIsInputName(true);
+            findCommand.setIsInputEmployeeId(false);
+        } else if (!isInputName && isInputEmployeeId) {
+            findCommand.setIsInputName(false);
+            findCommand.setIsInputEmployeeId(true);
+        }
+
+        return findCommand;
     }
 
+    /**
+     * Checks whether the given keyword is a valid name
+     */
+    public boolean isNameValid(String keyword) {
+        if (!Name.isValidName(keyword)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks whether the given keyword is a valid employeeId
+     */
+    public boolean isEmployeeIdValid(String keyword) {
+        if (!EmployeeId.isValidEmployeeId(keyword)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Check whether given keyword is a name or employeeId
+     */
+    public void isNameOrEmployeeId(String keyword) {
+        if (Character.isLetter(keyword.charAt(INDEX_FIRST_CHARACTER))) {
+            isInputName = isNameValid(keyword);
+            isInputEmployeeId = false;
+        } else if (Character.isDigit(keyword.charAt(INDEX_FIRST_CHARACTER))) {
+            isInputEmployeeId = isEmployeeIdValid(keyword);
+            isInputName = false;
+        }
+    }
+
+    
 }
