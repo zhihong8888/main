f30e0f34c892a5d82f347d512d9deb26e28e9625
LimYiSheng <e0176969@u.nus.edu>
2018-10-31 23:37:01 +0800

    Updated to fix travis

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,13 +10,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_NAME_CONSTRAINTS =
-            "Names should only contain alphabets and spaces, and it should not be blank";
+            "Names should only contain alphabets and spaces, it should not be blank and "
+            + "should at least be 3 characters long";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String NAME_VALIDATION_REGEX = "[A-Za-z ]+";
+    public static final String NAME_VALIDATION_REGEX = "[A-Za-z ]{3,}";
 
     public final String fullName;
 
