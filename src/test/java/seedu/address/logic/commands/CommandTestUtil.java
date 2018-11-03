014e32078130567028f90673330a4a2189d5982b
ChuaZhenWei <e0176888@u.nus.edu>
2018-11-03 22:00:11 +0800

    Test files

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -54,6 +54,8 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_SALARY_AMY = "8000.00";
     public static final String VALID_SALARY_BOB = "1000.00";
+    public static final String VALID_BONUS_AMY = "16000.00";
+    public static final String VALID_BONUS_BOB = "2000.00";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
 
---
+++ src/test/java/seedu/address/logic/commands/ModifyPayCommandTest.java
@@ -0,0 +1,356 @@
+package seedu.address.logic.commands;
+
+import org.junit.Test;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.commands.ModifyPayCommand.ModSalaryDescriptor;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.addressbook.AddressBook;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.EditPersonDescriptorBuilder;
+import seedu.address.testutil.ModSalaryDescriptorBuilder;
+import seedu.address.testutil.PersonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_BONUS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_SALARY_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.TypicalExpenses.getTypicalExpensesList;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalRecruitments.getTypicalRecruitmentList;
+import static seedu.address.testutil.schedule.TypicalSchedules.getTypicalScheduleList;
+
+/**
+ * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for EditCommand.
+ */
+public class ModifyPayCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), getTypicalExpensesList(), getTypicalScheduleList(),
+             getTypicalRecruitmentList(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute_allFieldsSpecifiedUnfilteredList_success() {
+        Person editedPerson = new PersonBuilder().build();
+        ModSalaryDescriptor descriptor = new ModSalaryDescriptorBuilder(editedPerson).build();
+        ModifyPayCommand modifyPayCommand = new ModifyPayCommand(INDEX_FIRST_PERSON, descriptor);
+
+        String expectedMessage = String.format(ModifyPayCommand.MESSAGE_MODIFIED_SUCCESS, editedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), model.getExpensesList(),
+                model.getScheduleList(), model.getRecruitmentList(), new UserPrefs());
+        expectedModel.updatePerson(model.getFilteredPersonList().get(0), editedPerson);
+        expectedModel.commitAddressBook();
+
+        try {
+            assertCommandSuccess(modifyPayCommand, model, commandHistory, expectedMessage, expectedModel);
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        }
+    }
+
+    @Test
+    public void execute_someFieldsSpecifiedUnfilteredList_success() {
+        Index indexLastPerson = Index.fromOneBased(model.getFilteredPersonList().size());
+        Person lastPerson = model.getFilteredPersonList().get(indexLastPerson.getZeroBased());
+
+        PersonBuilder personInList = new PersonBuilder(lastPerson);
+        Person modifiedPerson = personInList.withSalary(VALID_SALARY_BOB).withBonus(VALID_BONUS_BOB).build();
+
+        ModSalaryDescriptor descriptor = new ModSalaryDescriptorBuilder().withSalary(VALID_SALARY_BOB)
+                .withBonus(VALID_BONUS_BOB).build();
+        ModifyPayCommand modifyPayCommand = new ModifyPayCommand(indexLastPerson, descriptor);
+
+        String expectedMessage = String.format(ModifyPayCommand.MESSAGE_MODIFIED_SUCCESS, modifiedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), model.getExpensesList(),
+                model.getScheduleList(), model.getRecruitmentList(), new UserPrefs());
+        expectedModel.updatePerson(lastPerson, modifiedPerson);
+        expectedModel.commitAddressBook();
+
+        try {
+            assertCommandSuccess(modifyPayCommand, model, commandHistory, expectedMessage, expectedModel);
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        }
+    }
+
+    @Test
+    public void execute_noFieldSpecifiedUnfilteredList_success() {
+        ModifyPayCommand modifyPayCommand = new ModifyPayCommand(INDEX_FIRST_PERSON, new ModSalaryDescriptor());
+        Person editedPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+
+        String expectedMessage = String.format(ModifyPayCommand.MESSAGE_MODIFIED_SUCCESS, editedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), model.getExpensesList(),
+                model.getScheduleList(), model.getRecruitmentList(), new UserPrefs());
+        expectedModel.commitAddressBook();
+
+        try {
+            assertCommandSuccess(modifyPayCommand, model, commandHistory, expectedMessage, expectedModel);
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        }
+    }
+
+    @Test
+    public void execute_filteredList_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Person personInFilteredList = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person modifiedPerson = new PersonBuilder(personInFilteredList).withSalary(VALID_SALARY_BOB).build();
+        ModifyPayCommand modifyPayCommand = new ModifyPayCommand(INDEX_FIRST_PERSON,
+                new ModSalaryDescriptorBuilder().withSalary(VALID_SALARY_BOB).build());
+
+        String expectedMessage = String.format(ModifyPayCommand.MESSAGE_MODIFIED_SUCCESS, modifiedPerson);
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), model.getExpensesList(),
+                model.getScheduleList(), model.getRecruitmentList(), new UserPrefs());
+        expectedModel.updatePerson(model.getFilteredPersonList().get(0), modifiedPerson);
+        expectedModel.commitAddressBook();
+
+        try {
+            assertCommandSuccess(modifyPayCommand, model, commandHistory, expectedMessage, expectedModel);
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        }
+    }
+
+    @Test
+    public void execute_duplicateEmailUnfilteredList_failure() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        ModSalaryDescriptor descriptor = new ModSalaryDescriptorBuilder(firstPerson).build();
+        ModifyPayCommand modifyPayCommand = new ModifyPayCommand(INDEX_SECOND_PERSON, descriptor);
+
+        assertCommandFailure(modifyPayCommand, model, commandHistory, EditCommand.MESSAGE_DUPLICATE_EMAIL);
+    }
+
+    @Test
+    public void execute_duplicateEmailFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        // edit person in filtered list into a duplicate in address book
+        Person personInList = model.getAddressBook().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
+                new EditPersonDescriptorBuilder(personInList).build());
+
+        assertCommandFailure(editCommand, model, commandHistory, EditCommand.MESSAGE_DUPLICATE_EMAIL);
+    }
+
+    @Test
+    public void execute_duplicatePhoneUnfilteredList_failure() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(firstPerson)
+                .withEmail("Hello@example.com").build();
+        EditCommand editCommand = new EditCommand(INDEX_SECOND_PERSON, descriptor);
+
+        assertCommandFailure(editCommand, model, commandHistory, EditCommand.MESSAGE_DUPLICATE_PHONE);
+    }
+
+    @Test
+    public void execute_duplicatePhoneFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        // edit person in filtered list into a duplicate in address book
+        Person personInList = model.getAddressBook().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
+                new EditPersonDescriptorBuilder(personInList).withEmail("Hello@example.com").build());
+
+        assertCommandFailure(editCommand, model, commandHistory, EditCommand.MESSAGE_DUPLICATE_PHONE);
+    }
+
+    @Test
+    public void execute_duplicatePersonUnfilteredList_failure() {
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(firstPerson)
+                .withEmail("hello@example.com").withPhone("123456").build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, descriptor);
+
+        assertCommandFailure(editCommand, model, commandHistory, EditCommand.MESSAGE_DUPLICATE_PERSON);
+    }
+
+    @Test
+    public void execute_duplicatePersonFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_SECOND_PERSON);
+
+        // edit person in filtered list into a duplicate in address book
+        Person personInList = model.getAddressBook().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
+                new EditPersonDescriptorBuilder(personInList).withName("Alice Pauline")
+                        .withEmail("Hello@example.com").withPhone("88888888").build());
+
+        assertCommandFailure(editCommand, model, commandHistory, EditCommand.MESSAGE_DUPLICATE_PERSON);
+    }
+
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build();
+        EditCommand editCommand = new EditCommand(outOfBoundIndex, descriptor);
+
+        assertCommandFailure(editCommand, model, commandHistory, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    /**
+     * Edit filtered list where index is larger than size of filtered list,
+     * but smaller than size of address book
+     */
+    @Test
+    public void execute_invalidPersonIndexFilteredList_failure() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        EditCommand editCommand = new EditCommand(outOfBoundIndex,
+                new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build());
+
+        assertCommandFailure(editCommand, model, commandHistory, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void executeUndoRedo_validIndexUnfilteredList_success() throws Exception {
+        Person editedPerson = new PersonBuilder().build();
+        Person personToEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(editedPerson).build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, descriptor);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), model.getExpensesList(),
+                model.getScheduleList(), model.getRecruitmentList(), new UserPrefs());
+        expectedModel.updatePerson(personToEdit, editedPerson);
+        expectedModel.commitAddressBook();
+
+        // edit -> first person edited
+        editCommand.execute(model, commandHistory);
+
+        // undo -> reverts addressbook back to previous state and filtered person list to show all persons
+        expectedModel.undoAddressBook();
+        assertCommandSuccess(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+
+        // redo -> same first person edited again
+        expectedModel.redoAddressBook();
+        assertCommandSuccess(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void executeUndoRedo_invalidIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build();
+        EditCommand editCommand = new EditCommand(outOfBoundIndex, descriptor);
+
+        // execution failed -> address book state not added into model
+        assertCommandFailure(editCommand, model, commandHistory, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+
+        // single address book state in model -> undoCommand and redoCommand fail
+        assertCommandFailure(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_FAILURE);
+    }
+
+    /**
+     * 1. Edits a {@code Person} from a filtered list.
+     * 2. Undo the edit.
+     * 3. The unfiltered list should be shown now. Verify that the index of the previously edited person in the
+     * unfiltered list is different from the index at the filtered list.
+     * 4. Redo the edit. This ensures {@code RedoCommand} edits the person object regardless of indexing.
+     */
+    @Test
+    public void executeUndoRedo_validIndexFilteredList_samePersonEdited() throws Exception {
+        Person editedPerson = new PersonBuilder(BENSON).build();
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(editedPerson).build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, descriptor);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), model.getExpensesList(),
+                model.getScheduleList(), model.getRecruitmentList(), new UserPrefs());
+
+        showPersonAtIndex(model, INDEX_SECOND_PERSON);
+        Person personToEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        expectedModel.updatePerson(personToEdit, editedPerson);
+        expectedModel.commitAddressBook();
+
+        // edit -> edits second person in unfiltered person list / first person in filtered person list
+        editCommand.execute(model, commandHistory);
+
+        // undo -> reverts addressbook back to previous state and filtered person list to show all persons
+        expectedModel.undoAddressBook();
+        assertCommandSuccess(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+
+        assertNotEquals(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()), personToEdit);
+        // redo -> edits same second person in unfiltered person list
+        expectedModel.redoAddressBook();
+        assertCommandSuccess(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        final EditCommand standardCommand = new EditCommand(INDEX_FIRST_PERSON, DESC_AMY);
+
+        // same values -> returns true
+        EditPersonDescriptor copyDescriptor = new EditPersonDescriptor(DESC_AMY);
+        EditCommand commandWithSameValues = new EditCommand(INDEX_FIRST_PERSON, copyDescriptor);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new EditCommand(INDEX_SECOND_PERSON, DESC_AMY)));
+
+        // different descriptor -> returns false
+        assertFalse(standardCommand.equals(new EditCommand(INDEX_FIRST_PERSON, DESC_BOB)));
+    }
+
+    /**
+     * Executes the given {@code command}, confirms that <br>
+     * - a {@code CommandException} is thrown <br>
+     * - the CommandException message matches {@code expectedMessage} <br>
+     * - the address book in the {@code actualModel} remain unchanged <br>
+     * - {@code actualCommandHistory} remains unchanged.
+     * - filteredPersonList is changed therefore a different implementation of assertCommandFailure is created
+     */
+    public static void assertCommandFailure(Command command, Model actualModel, CommandHistory actualCommandHistory,
+                                            String expectedMessage) {
+        // we are unable to defensively copy the model for comparison later, so we can
+        // only do so by copying its components.
+        AddressBook expectedAddressBook = new AddressBook(actualModel.getAddressBook());
+
+        CommandHistory expectedCommandHistory = new CommandHistory(actualCommandHistory);
+
+        try {
+            command.execute(actualModel, actualCommandHistory);
+            throw new AssertionError("The expected CommandException was not thrown.");
+        } catch (CommandException e) {
+            assertEquals(expectedMessage, e.getMessage());
+            assertEquals(expectedAddressBook, actualModel.getAddressBook());
+            List<Person> expectedFilteredList = new ArrayList<>(actualModel.getFilteredPersonList());
+            assertEquals(expectedFilteredList, actualModel.getFilteredPersonList());
+            assertEquals(expectedCommandHistory, actualCommandHistory);
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        }
+    }
+
+}
