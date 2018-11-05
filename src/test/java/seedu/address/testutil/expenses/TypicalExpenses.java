2e84ee97f15469f365e5672af392a799e4f7d7c8
XiiaoPanda <vccx94@live.com.sg>
2018-11-05 17:57:39 +0800

    Typical expenses testcase

---
+++ src/test/java/seedu/address/testutil/expenses/TypicalExpenses.java
@@ -0,0 +1,61 @@
+package seedu.address.testutil.expenses;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.expenses.Expenses;
+import seedu.address.model.expenses.ExpensesList;
+
+/**
+ * A utility class containing a list of {@code schedule} objects to be used in tests.
+ */
+public class TypicalExpenses {
+
+    public static final String VALID_EMPLOYEEID_ALICE = "000001";
+    public static final String VALID_EXPENSES_AMOUNT_ALICE = "6000";
+    public static final String VALID_TRAVEL_EXPENSES_ALICE = "1000";
+    public static final String VALID_MEDICAL_EXPENSES_ALICE = "2000";
+    public static final String VALID_MISCELLANEOUS_EXPENSES_ALICE = "3000";
+
+    public static final Expenses ALICE_WORK = new ExpensesBuilder().withEmployeeId("000001")
+            .withTravelExpenses("123").withMedicalExpenses("123").withMiscellaneousExpenses("123").build();
+    public static final Expenses BENSON_WORK = new ExpensesBuilder().withEmployeeId("000002")
+            .withTravelExpenses("123").withMedicalExpenses("123").withMiscellaneousExpenses("123").build();
+    public static final Expenses CARL_WORK = new ExpensesBuilder().withEmployeeId("000003")
+            .withTravelExpenses("123").withMedicalExpenses("123").withMiscellaneousExpenses("123").build();
+    public static final Expenses DANIEL_LEAVE = new ExpensesBuilder().withEmployeeId("000004")
+            .withTravelExpenses("123").withMedicalExpenses("123").withMiscellaneousExpenses("123").build();
+    public static final Expenses ELLE_LEAVE = new ExpensesBuilder().withEmployeeId("000005")
+            .withTravelExpenses("123").withMedicalExpenses("123").withMiscellaneousExpenses("123").build();
+    public static final Expenses FIONA_LEAVE = new ExpensesBuilder().withEmployeeId("000006")
+            .withTravelExpenses("123").withMedicalExpenses("123").withMiscellaneousExpenses("123").build();
+    public static final Expenses GEORGE_LEAVE = new ExpensesBuilder().withEmployeeId("000007")
+            .withTravelExpenses("123").withMedicalExpenses("123").withMiscellaneousExpenses("123").build();
+
+    // Manually added - Person's details found in {@code CommandTestUtil}
+    public static final Expenses AMY = new ExpensesBuilder().withEmployeeId("000010")
+            .withTravelExpenses("123").withMedicalExpenses("123").withMiscellaneousExpenses("123").build();
+    public static final Expenses BOB = new ExpensesBuilder().withEmployeeId("000011")
+            .withTravelExpenses("123").withMedicalExpenses("123").withMiscellaneousExpenses("123").build();
+
+    public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
+
+    private TypicalExpenses() {} // prevents instantiation
+    /**
+     * Returns an {@code ScheduleList} with all the typical schedules.
+     */
+    public static ExpensesList getTypicalExpensesList() {
+        ExpensesList el = new ExpensesList();
+        for (Expenses expenses : getTypicalExpenses()) {
+            el.addExpenses(expenses);
+        }
+        return el;
+    }
+
+    public static List<Expenses> getTypicalExpenses() {
+        return new ArrayList<>(Arrays.asList(ALICE_WORK, BENSON_WORK, CARL_WORK, DANIEL_LEAVE,
+                ELLE_LEAVE, FIONA_LEAVE, GEORGE_LEAVE));
+    }
+
+}
