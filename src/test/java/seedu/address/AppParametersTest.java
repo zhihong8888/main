be727d80f4f22bea01d53197478285fb6f2b2bbe
zhihong8888 <zhihong8888@gmail.com>
2018-11-06 13:47:35 +0800

    app parameters

--- src/test/java/seedu/address/AppParametersTest.java
+++ src/test/java/seedu/address/AppParametersTest.java
@@ -1,8 +1,18 @@
 package seedu.address;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -54,5 +64,113 @@ public class AppParametersTest {
         public Map<String, String> getNamed() {
             return Collections.unmodifiableMap(namedParameters);
         }
+    }
+
+    @Test
+    public void equals() {
+        // same object -> returns true
+        assertTrue(expected.equals(expected));
+
+        // null -> returns false
+        assertFalse(expected == null);
+    }
+
+
+    @Test
+    public void appParametersComparable_nullAppParameters_hashCodeIsCorrect() {
+        Path path = new Path() {
+            @Override
+            public FileSystem getFileSystem() {
+                return null;
+            }
+
+            @Override
+            public boolean isAbsolute() {
+                return false;
+            }
+
+            @Override
+            public Path getRoot() {
+                return null;
+            }
+
+            @Override
+            public Path getFileName() {
+                return null;
+            }
+
+            @Override
+            public Path getParent() {
+                return null;
+            }
+
+            @Override
+            public int getNameCount() {
+                return 0;
+            }
+
+            @Override
+            public Path getName(int index) {
+                return null;
+            }
+
+            @Override
+            public Path subpath(int beginIndex, int endIndex) {
+                return null;
+            }
+
+            @Override
+            public boolean startsWith(Path other) {
+                return false;
+            }
+
+            @Override
+            public boolean endsWith(Path other) {
+                return false;
+            }
+
+            @Override
+            public Path normalize() {
+                return null;
+            }
+
+            @Override
+            public Path resolve(Path other) {
+                return null;
+            }
+
+            @Override
+            public Path relativize(Path other) {
+                return null;
+            }
+
+            @Override
+            public URI toUri() {
+                return null;
+            }
+
+            @Override
+            public Path toAbsolutePath() {
+                return null;
+            }
+
+            @Override
+            public Path toRealPath(LinkOption... options) throws IOException {
+                return null;
+            }
+
+            @Override
+            public WatchKey register(WatchService watcher, WatchEvent.Kind<?>[] events,
+                                     WatchEvent.Modifier... modifiers) throws IOException {
+                return null;
+            }
+
+            @Override
+            public int compareTo(Path other) {
+                return 0;
+            }
+        };
+        expected.setConfigPath(path);
+        assertEquals(path.hashCode(), expected.hashCode());
     }
 }
