7ea0a600aa27c7c992e316c9e3bf98fd336e2df5
zhihong8888 <zhihong8888@gmail.com>
2018-11-07 15:08:55 +0800

    clear schedule command

--- src/test/java/seedu/address/logic/commands/ClearScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearScheduleCommandTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalExpenses.getTypicalExpensesList;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
@@ -9,27 +10,28 @@ import static seedu.address.testutil.schedule.TypicalSchedules.getTypicalSchedul
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.schedule.ScheduleList;
+import seedu.address.testutil.Assert;
 
 public class ClearScheduleCommandTest {
 
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
-    public void execute_emptyAddressBookScheduleList_success() {
+    public void execute_emptyScheduleList_throwsCommandException() {
         Model model = new ModelManager();
         Model expectedModel = new ModelManager();
-        expectedModel.commitAddressBook();
-        try {
-            assertCommandSuccess(new ClearCommand(), model,
-                    commandHistory, ClearCommand.MESSAGE_SUCCESS, expectedModel);
-        } catch (ParseException pe) {
-            pe.printStackTrace();
-        }
+        expectedModel.commitScheduleList();
+
+        ClearScheduleCommand clearScheduleCommand = new ClearScheduleCommand();
+        Assert.assertThrows(CommandException.class, () -> clearScheduleCommand.execute(model, commandHistory));
+        assertCommandFailure(new ClearScheduleCommand(), model,
+                    commandHistory, ClearScheduleCommand.MESSAGE_FAILURE_CLEARED);
     }
 
     @Test
