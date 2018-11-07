5544d8802572d74ed4e9cdd4ee163aa2de64de0b
zhihong8888 <zhihong8888@gmail.com>
2018-11-07 14:48:01 +0800

    clear schedule command tests

---
+++ src/test/java/seedu/address/logic/commands/ClearScheduleCommandTest.java
@@ -0,0 +1,53 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalExpenses.getTypicalExpensesList;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalRecruitments.getTypicalRecruitmentList;
+import static seedu.address.testutil.schedule.TypicalSchedules.getTypicalScheduleList;
+
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.schedule.ScheduleList;
+
+public class ClearScheduleCommandTest {
+
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute_emptyAddressBookScheduleList_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+        expectedModel.commitAddressBook();
+        try {
+            assertCommandSuccess(new ClearCommand(), model,
+                    commandHistory, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        }
+    }
+
+    @Test
+    public void execute_nonEmptyScheduleList_success() {
+        Model model = new ModelManager(getTypicalAddressBook(), getTypicalExpensesList(), getTypicalScheduleList(),
+                getTypicalRecruitmentList(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), getTypicalExpensesList(),
+                getTypicalScheduleList(), getTypicalRecruitmentList(), new UserPrefs());
+
+        expectedModel.resetScheduleListData(new ScheduleList());
+        expectedModel.commitScheduleList();
+
+        try {
+            assertCommandSuccess(new ClearScheduleCommand(), model,
+                    commandHistory, ClearScheduleCommand.MESSAGE_SUCCESS, expectedModel);
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        }
+    }
+
+}
