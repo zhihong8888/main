f99b8e438f3932b074af5bbefa858a79bfd7f9cd
XiiaoPanda <vccx94@live.com.sg>
2018-11-07 23:56:01 +0800

    Travis test

--- src/test/java/seedu/address/logic/commands/ModifyPayCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ModifyPayCommandTest.java
@@ -10,7 +10,7 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_BONUS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_SALARY_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
-import static seedu.address.testutil.TypicalExpenses.getTypicalExpensesList;
+import static seedu.address.testutil.expenses.TypicalExpenses.getTypicalExpensesList;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.BENSON;
