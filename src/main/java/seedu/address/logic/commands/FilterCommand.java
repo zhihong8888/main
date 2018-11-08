8776635023a2c16db908cd7a43c61adb3185cc2e
LimYiSheng <e0176969@u.nus.edu>
2018-11-09 00:59:44 +0800

    Updated FilterCommand to now filter out the listed employees' schedule and expenses only

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_DEPARTMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POSITION;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,13 +13,17 @@ import java.util.Set;
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
+import seedu.address.model.expenses.EmployeeIdExpensesContainsKeywordsPredicate;
 import seedu.address.model.person.DepartmentContainsKeywordsPredicate;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.PositionContainsKeywordsPredicate;
+import seedu.address.model.schedule.EmployeeIdScheduleContainsKeywordsPredicate;
 
 /**
  * Filters and lists all persons in address book whose department and/or position contains any of the argument keywords.
  * Keyword matching is case insensitive.
+ * The list is sorted either in ascending or descending name order based on the user's input
  */
 public class FilterCommand extends Command {
 
@@ -64,6 +69,38 @@ public class FilterCommand extends Command {
         this.positionPredicate = positionPredicate;
     }
 
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+        String allAvailableDepartments = listAvailableDepartments(model);
+        String allAvailablePositions = listAvailablePositions(model);
+
+        if (isDepartmentPrefixPresent && !isPositionPrefixPresent) {
+            model.updateFilteredPersonList(departmentPredicate, sortOrder);
+        } else if (isPositionPrefixPresent && !isDepartmentPrefixPresent) {
+            model.updateFilteredPersonList(positionPredicate, sortOrder);
+        } else if (isDepartmentPrefixPresent && isPositionPrefixPresent) {
+            model.updateFilteredPersonList(departmentPredicate.and(positionPredicate), sortOrder);
+        }
+
+        EmployeeIdExpensesContainsKeywordsPredicate expensesPredicate = generateEmployeeIdExpensesPredicate(model);
+        EmployeeIdScheduleContainsKeywordsPredicate schedulePredicate = generateEmployeeIdSchedulePredicate(model);
+        model.updateFilteredExpensesList(expensesPredicate);
+        model.updateFilteredScheduleList(schedulePredicate);
+
+        return new CommandResult(feedbackToUser(model, allAvailableDepartments, allAvailablePositions));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FilterCommand // instanceof handles nulls
+                && (departmentPredicate.and(positionPredicate).equals(((FilterCommand) other)
+                .departmentPredicate.and(positionPredicate))
+                || (departmentPredicate.equals(((FilterCommand) other).departmentPredicate))
+                || (positionPredicate.equals(((FilterCommand) other).positionPredicate)))); // state check
+    }
+
     /**
      * Gets all the departments available for filtering within the address book currently
      * and converts it into a string
@@ -107,8 +144,47 @@ public class FilterCommand extends Command {
     }
 
     /**
+     * Generates a predicate that holds the employee ids of matched names or employee id via find command
+     * for expenses list to be updated
+     * @param model
+     * @return EmployeeIdExpensesContainsKeywordsPredicate that holds true to all employee ids of matched name
+     *      * or employee id
+     */
+    public EmployeeIdExpensesContainsKeywordsPredicate generateEmployeeIdExpensesPredicate(Model model) {
+        List<Person> getFilteredList = model.getFilteredPersonList();
+        List<String> matchedEmployeeIds = new ArrayList<>();
+
+        for (Person person : getFilteredList) {
+            matchedEmployeeIds.add(person.getEmployeeId().value);
+        }
+
+        return new EmployeeIdExpensesContainsKeywordsPredicate(matchedEmployeeIds);
+    }
+
+    /**
+     * Generates a predicate that holds the employee ids of matched names or employee id via find command
+     * for schedule list to be updated
+     * @param model
+     * @return EmployeeIdScheduleContainsKeywordsPredicate that holds true to all employee ids of matched name
+     * or employee id
+     */
+    public EmployeeIdScheduleContainsKeywordsPredicate generateEmployeeIdSchedulePredicate(Model model) {
+        List<Person> getFilteredList = model.getFilteredPersonList();
+        List<String> matchedEmployeeIds = new ArrayList<>();
+
+        for (Person person : getFilteredList) {
+            matchedEmployeeIds.add(person.getEmployeeId().value);
+        }
+
+        return new EmployeeIdScheduleContainsKeywordsPredicate(matchedEmployeeIds);
+    }
+
+    /**
      * Generates the feedback to be shown on CLI to the user
      * @param model
+     * @param allAvailableDepartments
+     * @param allAvailablePositions
+     * @return the message to be printed on the CLI to the user
      */
     public String feedbackToUser(Model model, String allAvailableDepartments, String allAvailablePositions) {
         String toBeConcatenated = "";
@@ -123,32 +199,5 @@ public class FilterCommand extends Command {
 
         return String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size())
                 + toBeConcatenated;
-    }
-
-    @Override
-    public CommandResult execute(Model model, CommandHistory history) {
-        requireNonNull(model);
-        String allAvailableDepartments = listAvailableDepartments(model);
-        String allAvailablePositions = listAvailablePositions(model);
-
-        if (isDepartmentPrefixPresent && !isPositionPrefixPresent) {
-            model.updateFilteredPersonList(departmentPredicate, sortOrder);
-        } else if (isPositionPrefixPresent && !isDepartmentPrefixPresent) {
-            model.updateFilteredPersonList(positionPredicate, sortOrder);
-        } else if (isDepartmentPrefixPresent && isPositionPrefixPresent) {
-            model.updateFilteredPersonList(departmentPredicate.and(positionPredicate), sortOrder);
-        }
-
-        return new CommandResult(feedbackToUser(model, allAvailableDepartments, allAvailablePositions));
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof FilterCommand // instanceof handles nulls
-                && (departmentPredicate.and(positionPredicate).equals(((FilterCommand) other)
-                .departmentPredicate.and(positionPredicate))
-                || (departmentPredicate.equals(((FilterCommand) other).departmentPredicate))
-                || (positionPredicate.equals(((FilterCommand) other).positionPredicate)))); // state check
     }
 }
