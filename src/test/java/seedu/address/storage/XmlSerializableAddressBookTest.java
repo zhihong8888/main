cf258ad4f8f29788cfa1db6642002c218537d851
LimYiSheng <e0176969@u.nus.edu>
2018-11-11 03:37:39 +0800

    Updated XmlSerializableAddressBookTest

--- src/test/java/seedu/address/storage/XmlSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/XmlSerializableAddressBookTest.java
@@ -1,6 +1,8 @@
 package seedu.address.storage;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,12 +15,14 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.XmlUtil;
 import seedu.address.model.addressbook.AddressBook;
 import seedu.address.storage.addressbook.XmlSerializableAddressBook;
+import seedu.address.storage.schedule.XmlSerializableScheduleList;
 import seedu.address.testutil.TypicalPersons;
 
 public class XmlSerializableAddressBookTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "XmlSerializableAddressBookTest");
     private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBook.xml");
+    private static final Path TYPICAL_PERSONS_FILE_COPY = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBookCopy.xml");
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.xml");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.xml");
 
@@ -49,6 +53,31 @@ public class XmlSerializableAddressBookTest {
         thrown.expect(IllegalValueException.class);
         thrown.expectMessage(XmlSerializableAddressBook.MESSAGE_DUPLICATE_PERSON);
         dataFromFile.toModelType();
+    }
+
+    @Test
+    public void equals_samePerson_true() throws Exception {
+        XmlSerializableAddressBook dataFromFile = XmlUtil.getDataFromFile(TYPICAL_PERSONS_FILE,
+                XmlSerializableAddressBook.class);
+        // same object -> returns true
+        assertTrue(dataFromFile.equals(dataFromFile));
+    }
+    @Test
+    public void equals_null_false() throws Exception {
+        XmlSerializableAddressBook dataFromFile = XmlUtil.getDataFromFile(TYPICAL_PERSONS_FILE,
+                XmlSerializableAddressBook.class);
+        // null object -> returns false
+        assertFalse(dataFromFile.equals(null));
+    }
+
+    @Test
+    public void equals_samePersonInDifferentFile_true() throws Exception {
+        // same schedules in different file -> returns true
+        XmlSerializableAddressBook dataFromFile = XmlUtil.getDataFromFile(TYPICAL_PERSONS_FILE,
+                XmlSerializableAddressBook.class);
+        XmlSerializableAddressBook dataFromFileCopy = XmlUtil.getDataFromFile(TYPICAL_PERSONS_FILE_COPY,
+                XmlSerializableAddressBook.class);
+        assertTrue(dataFromFile.equals(dataFromFileCopy));
     }
 
 }
