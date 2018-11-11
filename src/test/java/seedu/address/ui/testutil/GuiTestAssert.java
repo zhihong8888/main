59e148dba4186ddb65ba1bfd11dc608501c60358
XiiaoPanda <vccx94@live.com.sg>
2018-11-11 13:52:56 +0800

    testcase for expenses UI

--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import guitests.guihandles.ExpensesCardHandle;
 import guitests.guihandles.PersonCardHandle;
 import guitests.guihandles.PersonListPanelHandle;
 import guitests.guihandles.ResultDisplayHandle;
 import guitests.guihandles.ScheduleCardHandle;
+import seedu.address.model.expenses.Expenses;
 import seedu.address.model.person.Person;
 import seedu.address.model.schedule.Schedule;
 
@@ -39,6 +41,18 @@ public class GuiTestAssert {
     }
 
     /**
+     * Asserts that {@code actualCard} displays the same values as {@code expectedCard}.
+     */
+    public static void assertCardEqualsExpenses(ExpensesCardHandle expectedCard, ExpensesCardHandle actualCard) {
+        assertEquals(expectedCard.getId(), actualCard.getId());
+        assertEquals(expectedCard.getEmployeeId(), actualCard.getEmployeeId());
+        assertEquals(expectedCard.getExpensesamount(), actualCard.getExpensesamount());
+        assertEquals(expectedCard.getTravelexpenses(), actualCard.getTravelexpenses());
+        assertEquals(expectedCard.getMedicalexpenses(), actualCard.getMedicalexpenses());
+        assertEquals(expectedCard.getMiscellaneousexpenses(), actualCard.getMiscellaneousexpenses());
+    }
+
+    /**
      * Asserts that {@code actualCard} displays the details of {@code expectedPerson}.
      */
     public static void assertCardDisplaysPerson(Person expectedPerson, PersonCardHandle actualCard) {
@@ -63,6 +77,18 @@ public class GuiTestAssert {
         assertEquals(expectedSchedule.getEmployeeId().value, actualCard.getEmployeeId());
         assertEquals(expectedSchedule.getScheduleDate().value, actualCard.getDate());
         assertEquals(expectedSchedule.getType().value, expectedSchedule.getType().value);
+    }
+
+    /**
+     * Asserts that {@code actualCard} displays the details of {@code expectedPerson}.
+     */
+    public static void assertCardDisplaysExpenses(Expenses expectedExpenses, ExpensesCardHandle actualCard) {
+        assertEquals(expectedExpenses.getEmployeeId().value, actualCard.getEmployeeId());
+        assertEquals(expectedExpenses.getExpensesAmount().expensesAmount, actualCard.getExpensesamount());
+        assertEquals(expectedExpenses.getTravelExpenses().travelExpenses, actualCard.getTravelexpenses());
+        assertEquals(expectedExpenses.getMedicalExpenses().medicalExpenses, actualCard.getMedicalexpenses());
+        assertEquals(expectedExpenses.getMiscellaneousExpenses().miscellaneousExpenses, actualCard
+                .getMiscellaneousexpenses());
     }
 
     /**
