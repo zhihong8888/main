e8535acb6fab844e7d68fccc08cff14e79342a32
XiiaoPanda <vccx94@live.com.sg>
2018-11-11 17:52:54 +0800

    update travis check

--- src/test/java/seedu/address/logic/commands/RemoveExpensesCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemoveExpensesCommandTest.java
@@ -6,13 +6,13 @@ import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showExpensesAtIndex;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EXPENSES;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_EXPENSES;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalRecruitments.getTypicalRecruitmentList;
-import static seedu.address.testutil.expenses.TypicalExpenses.getTypicalExpensesList;
+
 import static seedu.address.testutil.schedule.TypicalSchedules.getTypicalScheduleList;
 
 import org.junit.Test;
